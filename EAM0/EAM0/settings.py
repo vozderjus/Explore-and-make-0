58593984b8fef3272c213db35d867ab1a56f8f2a
vozderjus <memkased@gmail.com>
2026-04-07 20:48:56 +0500

    settings

--- EAM0/EAM0/settings.py
+++ EAM0/EAM0/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Asia/Yekaterinburg'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
